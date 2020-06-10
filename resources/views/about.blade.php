ae748ceb0df4be648d25cc7f072c9d38050cf985
Fabio Gadau <fabiogadau92@gmail.com>
2020-06-10 20:08:21 +0200

    laravel logic ok

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+{{-- ABOUT --}}
+
+@extends('layouts.main-layout')
+
+@section('content')
+   <h2>About</h2>
+   <a href="{{ 'index' }}">Return to home</a>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+{{-- HOME --}}
+
+@extends('layouts.main-layout')
+
+@section('content')
+   <h2>Home</h2>
+   <a href="{{ 'about' }}">About us</a>
+@endsection
